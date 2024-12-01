0b6dc31c141c8bf87700c9cf79231dab41000aeb
yebrong <officeguider@naver.com>
2024-12-01 19:54:09 +0900

    feat : Application

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.game.GameContext;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameContext gameContext = new GameContext();
+        gameContext.run();
     }
 }
